a775ba834507074e3c6e53d01ff53a80d354cd10
Tobias Grundel <tobias.grundel@hotmail.com>
2014-11-19 15:50:15 +0100

    AssetManager - First draft Waiting on proper rendering for testing purposes

---
+++ ProjectFlightSchool/Graphics/Static3dAsset.h
@@ -0,0 +1,20 @@
+#ifndef STATIC3DASSET_H
+#define STATIC3DASSET_H
+
+#include "AssetBase.h"
+
+struct Static3dAsset : public AssetBase
+{
+	private:
+
+	protected:
+	public:
+
+	private:
+	protected:
+	public:
+				Static3dAsset();
+	virtual		~Static3dAsset();
+};
+
+#endif

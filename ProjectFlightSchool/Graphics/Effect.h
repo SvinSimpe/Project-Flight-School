70ea3920d831031f90d6e6f90f95401ec73344ac
unknown <SvinSimpe>
2014-11-19 15:45:05 +0100

    Add Effect class When an effect (a set of shaders) is needed, a EffectInfo struct 	is filled out and passed to the Effect class initialize funcion. 	Effect contains pointers to all shader types and these will either 	be initialize or ignored upon be called by developer, depending on 	which flags has been turned on.

---
+++ ProjectFlightSchool/Graphics/Effect.h
@@ -0,0 +1,51 @@
+#ifndef _EFFECT_H_
+#define _EFFECT_H_
+
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <string>
+#include <fstream>
+
+#define SAFE_RELEASE( x ) if( x ) { ( x )->Release(); ( x ) = nullptr; }
+
+struct EffectInfo
+{
+	char* fileName = "";
+	bool isVertexShaderIncluded = false;
+	bool isHullShaderIncluded = false;
+	bool isDomainShaderIncluded = false;
+	bool isGeometryShaderIncluded = false;
+	bool isPixelShaderIncluded = false;
+
+	//Reset all struct variables
+	void Reset()
+	{
+		fileName = "";
+		isVertexShaderIncluded = false;
+		isHullShaderIncluded = false;
+		isDomainShaderIncluded = false;
+		isGeometryShaderIncluded = false;
+		isPixelShaderIncluded = false;
+	}
+};
+
+class Effect
+{
+private:
+	ID3D11InputLayout*		mInputLayout;
+	ID3D11VertexShader*		mVertexShader;
+	ID3D11HullShader*		mHullShader;
+	ID3D11DomainShader*		mDomainShader;
+	ID3D11GeometryShader*	mGeometryShader;
+	ID3D11PixelShader*		mPixelShader;
+
+private:
+	HRESULT CompileShader(char* shaderFile, char* pEntrypoint, char* pTarget, D3D10_SHADER_MACRO* pDefines, ID3DBlob** pCompiledShader);
+
+public:
+	HRESULT Intialize( ID3D11Device* device, EffectInfo effectInfo );
+	void Release();
+	Effect();
+	~Effect();
+};
+#endif

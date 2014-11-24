101872e3e2c4bddd31b8541f13d26aabd73ab032
maxilator <maxilator@github.com>
2014-11-24 17:03:45 +0100

    AnimationImport has been updated The animation import class got a structure for the animation data

--- ProjectFlightSchool/ProjectFlightSchool/ImportAnimation.h
+++ ProjectFlightSchool/ProjectFlightSchool/ImportAnimation.h
@@ -1,13 +1,39 @@
-#ifndef GAME_H
-#define GAME_H
+#ifndef IMPORTANIMATION_H
+#define IMPORTANIMATION_H
+
+#include <vector>
+
+struct animationData
+{
+	int nrOfJoints;
+	char animationName[128];
+	std::vector<jointAnimation> joints;
+};
+struct jointAnimation
+{
+	char jointName[128];
+	char parentName[128];
+	std::vector<int> keys;
+	std::vector<animationMatrix> matricies;
+};
+struct animationMatrix
+{
+	double matrixData[4][4];
+};
 
 class ImportAnimation
 {
+	//Class members
 	private:
-
+		animationData mTempAnimData;
 	protected:
 	public:
 	
+	//Class functions
+	private:
+	protected:
+	public:
+		animationData ImportBinaryAnimData(char* path);
 };
 
 #endif

ab1fa68ece39bfe8c02d53495bc5508ec019c71a
unknown <Mioa@github.com name = Mioa>
2014-12-05 09:07:42 +0100

    Timer class added 	-DeltaTime avaiblable for use thanks to timer class.

---
+++ ProjectFlightSchool/ProjectFlightSchool/Timer.h
@@ -0,0 +1,27 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+#include <Windows.h>
+
+class Timer
+{
+	private:
+		__int64		mCntsPerSec;
+		double		mSecsPerCnt;
+		__int64		mPrevTimeStamp;
+		float		mFps;
+	protected:
+	public:
+
+	private:
+	protected:
+	public:
+		float	GetDeltaTime();
+		float		GetFPS();
+
+		HRESULT	Initialize();
+		void	Release();
+				Timer();
+		virtual	~Timer();
+};
+#endif

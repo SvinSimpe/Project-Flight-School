6340dae99043fb24aeb892b9f878046e7035541d
unknown <johanson.simon@hotmail.com>
2014-12-16 22:13:55 +0100

    Add Phong Shading Phong shading implemented in Placeholders shaders. Light calculations 	is now included in the scene.

--- ProjectFlightSchool/Content/Effects/PlaceholderAni.hlsl
+++ ProjectFlightSchool/Content/Effects/PlaceholderAni.hlsl
@@ -25,9 +25,10 @@ struct VS_In
 
 struct VS_Out
 {
-	float4 position	: SV_POSITION;
-	float3 normal	: NORMAL;
-	float2 uv		: TEX;
+	float4 position			: SV_POSITION;
+	float3 worldPosition	: WORLD_POSITION;
+	float3 normal			: NORMAL;
+	float2 uv				: TEX;
 };
 
 VS_Out VS_main( VS_In input )
@@ -38,9 +39,10 @@ VS_Out VS_main( VS_In input )
 	for( int i = 0; i < 4; i++ )
 		transformedPos += mul( float4( input.position, 1.0f ), boneTransforms[input.jointIndex[i]] ).xyz * input.weights[i];
 
-	output.position = mul( float4( transformedPos, 1.0f ), worldMatrix );
-	output.position = mul( output.position, viewMatrix );
-	output.position = mul( output.position, projectionMatrix );
+	output.position			= mul( float4( transformedPos, 1.0f ), worldMatrix );
+	output.worldPosition	= output.position.xyz;
+	output.position			= mul( output.position, viewMatrix );
+	output.position			= mul( output.position, projectionMatrix );
 
 	output.normal	= mul( input.normal.xyz, (float3x3)worldMatrix );
 
@@ -53,9 +55,31 @@ VS_Out VS_main( VS_In input )
 float4 PS_main( VS_Out input ) : SV_TARGET0
 {
 	// Derp light to notice anything
-	float lightIntensity	= dot( input.normal, normalize( float3( 0.5f, 1.0f, -0.3f ) ) ); 
-	float3 color			= float3( 1.0f, 1.0f, 1.0f );
+	//float lightIntensity	= dot( input.normal, normalize( float3( 0.5f, 1.0f, -0.3f ) ) ); 
+	//float3 color			= float3( 1.0f, 1.0f, 1.0f );
 
-	return float4( color * lightIntensity, 1.0f );
+	//return float4( color * lightIntensity, 1.0f );
 	//return float4( input.normal, 1.0f );
+
+	//======== PHONG SHADING ===========
+	float4 ambient		= float4( 0.1f, 0.1f,  0.1f, 1.0f );
+	float4 diffuse		= float4( 1.0f, 0.45f, 1.0f, 1.0f );
+	float4 specular		= float4( 0.1f, 0.1f,  0.1f, 1.0f );
+	float4 color		= float4( 1.0f, 0.6f, 1.0f, 1.0f );
+
+	float3 lightDirection	= float3( 0.5f, 1.0f, -0.3f );
+
+	//Calculate diffuse factor
+	float diffuseFactor = max( 0, dot( input.normal, lightDirection ) );
+	diffuse				= float4( color.xyz * diffuseFactor, 1.0f );
+
+	//Calculate specular factor
+	float3 reflection		= normalize( reflect( -lightDirection, input.normal ) );
+	float3 viewVector		= normalize( cameraPosition - input.position ).xyz;
+	float specularFactor	= max( 0, dot( reflection, viewVector ) );
+	float specularPower		= 32.0f;
+	specular				= float4( color.xyz * pow( specularFactor, specularPower ), 1.0f );
+
+	float4 finalColor		= float4( ( ambient + diffuse + specular ) * color );
+	return finalColor;
 }

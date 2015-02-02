54d531b4bdb9343bf24ecfedc453ad765d19d96f
svekai <karinsve90@hotmail.com>
2015-02-02 14:42:23 +0100

    Fixed a bug in SoundBuffer::Release(), updated some code to code standard and changed the GetInstance to remove memory leaks.

--- ProjectFlightSchool/ProjectFlightSchool/SoundBufferHandler.cpp
+++ ProjectFlightSchool/ProjectFlightSchool/SoundBufferHandler.cpp
@@ -1,104 +1,110 @@
 #include "SoundBufferHandler.h"
 
-int SoundBufferHandler::Load3DBuffer(char* fileName)
+int SoundBufferHandler::Load3DBuffer( char* fileName )
 {
-	for (auto s : m3DSoundAssets)
+	for ( auto s : m3DSoundAssets )
 	{
-		if (s.GetFileName() == fileName)
+		if ( s.GetFileName() == fileName )
 		{
 			return s.GetID();
 		}
 	}
 	DS3DBuffer buffer;
-	buffer.Initialize(mDSDevice, fileName, m3DSoundAssets.size());
-	m3DSoundAssets.push_back(buffer);
-	return m3DSoundAssets.size() - 1;
+	int id = m3DSoundAssets.size();
+	buffer.Initialize( mDSDevice, fileName, id );
+	m3DSoundAssets.push_back( buffer );
+	return id;
 }
 
-int SoundBufferHandler::LoadBuffer(char* fileName)
+int SoundBufferHandler::LoadBuffer( char* fileName )
 {
-	for (auto s : mSoundAssets)
+	for ( auto s : mSoundAssets )
 	{
-		if (s.GetFileName() == fileName)
+		if ( s.GetFileName() == fileName )
 		{
 			return s.GetID();
 		}
 	}
 	DSBuffer buffer;
-	buffer.Initialize(mDSDevice, fileName, mSoundAssets.size());
-	mSoundAssets.push_back(buffer);
-	return mSoundAssets.size() - 1;
+	int id = mSoundAssets.size();
+	buffer.Initialize( mDSDevice, fileName, id );
+	mSoundAssets.push_back( buffer );
+	return id;
 }
 
-void SoundBufferHandler::Play(int SoundID)
+void SoundBufferHandler::Play( int SoundID )
 {
 	mSoundAssets.at(SoundID).PlayBuffer();
 }
 
-void SoundBufferHandler::Loop(int SoundID)
+void SoundBufferHandler::Loop( int SoundID )
 {
 	mSoundAssets.at(SoundID).PlayBufferLoop();
 }
 
-void SoundBufferHandler::StopLoop(int SoundID)
+void SoundBufferHandler::StopLoop( int SoundID )
 {
 	mSoundAssets.at(SoundID).StopBuffer();
 }
 
-void SoundBufferHandler::Play3D(int SoundID, XMFLOAT3 pos)
+void SoundBufferHandler::Play3D( int SoundID, XMFLOAT3 pos )
 {
-	m3DSoundAssets.at(SoundID).PlayBuffer(pos);
+	m3DSoundAssets.at(SoundID).PlayBuffer( pos );
 }
 
-void SoundBufferHandler::Loop3D(int SoundID, XMFLOAT3 pos)
+void SoundBufferHandler::Loop3D( int SoundID, XMFLOAT3 pos )
 {
-	m3DSoundAssets.at(SoundID).PlayBufferLoop(pos);
+	m3DSoundAssets.at(SoundID).PlayBufferLoop( pos );
 }
 
-void SoundBufferHandler::StopLoop3D(int SoundID)
+void SoundBufferHandler::StopLoop3D( int SoundID )
 {
 	m3DSoundAssets.at(SoundID).StopBuffer();
 }
 
 bool SoundBufferHandler::Initialize()
 {
-	EventManager::GetInstance()->AddListener(&SoundBufferHandler::EventListener, this, Event_Player_Update::GUID);
-	bool result = SUCCEEDED(InitDevice());
-	if (result)
+	EventManager::GetInstance()->AddListener( &SoundBufferHandler::EventListener, this, Event_Player_Update::GUID );
+	bool result = SUCCEEDED( InitDevice() );
+	if ( result )
 	{
 		result = GetDSListener();
 	}
-	if (!result)
+	if ( !result )
 	{
-		MessageBox(NULL, L"GetListener failed", L"Error", MB_OK);
+		MessageBox( NULL, L"GetListener failed", L"Error", MB_OK );
 	}
 	return result;
 }
 
 void SoundBufferHandler::Release()
 {
-	for (auto& s : mSoundAssets)
+	for ( auto& s : mSoundAssets )
 	{
 		s.Release();
 	}
 	mSoundAssets.clear();
-	for (auto& s : m3DSoundAssets)
+	for ( auto& s : m3DSoundAssets )
 	{
 		s.Release();
 	}
 	m3DSoundAssets.clear();
-	for (auto& s : mActiveSounds)
+	for ( auto& s : mActiveSounds )
 	{
 		s.Release();
 	}
 	mActiveSounds.clear();
-	for (auto& s : mActive3DSounds)
+	for ( auto& s : mActive3DSounds )
 	{
 		s.Release();
 	}
 	mActive3DSounds.clear();
 	mDSListener->Release();
-	mDSDevice->Release();
+	if ( mDSDevice )
+	{
+		mDSDevice->Release();
+		mDSDevice = 0;
+	}
 }
 
 SoundBufferHandler::SoundBufferHandler()
@@ -118,7 +124,7 @@ SoundBufferHandler::~SoundBufferHandler()
 
 HRESULT SoundBufferHandler::InitDevice()
 {
-    HRESULT hr = CoCreateInstance( CLSID_DirectSound8,
+    /*HRESULT hr = CoCreateInstance( CLSID_DirectSound8,
               NULL, 
               CLSCTX_INPROC_SERVER,
               IID_IDirectSound8,
@@ -131,7 +137,9 @@ HRESULT SoundBufferHandler::InitDevice()
     if ( FAILED( hr ) )
     {
       MessageBox( NULL, L"InitDevice failed", L"Error", MB_OK );
-    }
+    }*/
+
+	HRESULT hr = DirectSoundCreate8( NULL, &mDSDevice, NULL );
 
 	HWND hwnd = GetForegroundWindow();
 	hr = mDSDevice->SetCooperativeLevel( hwnd, DSSCL_PRIORITY );
@@ -144,7 +152,7 @@ HRESULT SoundBufferHandler::InitDevice()
 	return S_OK;
 }
 
-void SoundBufferHandler::EventListener(IEventPtr newEvent)
+void SoundBufferHandler::EventListener( IEventPtr newEvent )
 {
 	if (newEvent->GetEventType() == Event_Player_Update::GUID) // Add a remote player to the list when they connect
 	{
@@ -168,22 +176,18 @@ bool SoundBufferHandler::GetDSListener()
 	dsbd.dwSize = sizeof(DSBUFFERDESC);
 	dsbd.dwFlags = DSBCAPS_CTRL3D | DSBCAPS_PRIMARYBUFFER;
 
-	if (SUCCEEDED(hr = mDSDevice->CreateSoundBuffer(&dsbd, &lpdsbPrimary, NULL)))
+	if ( SUCCEEDED( hr = mDSDevice->CreateSoundBuffer( &dsbd, &lpdsbPrimary, NULL ) ) )
 	{
-		hr = lpdsbPrimary->QueryInterface(IID_IDirectSound3DListener8,
-			(LPVOID *)&mDSListener);
+		hr = lpdsbPrimary->QueryInterface( IID_IDirectSound3DListener8,
+			(LPVOID *)&mDSListener );
 		lpdsbPrimary->Release();
 	}
-	mDSListener->SetPosition(0, 0, 0, DS3D_IMMEDIATE);
-	return SUCCEEDED(hr);
+	mDSListener->SetPosition( 0, 0, 0, DS3D_IMMEDIATE );
+	return SUCCEEDED( hr );
 }
 
 SoundBufferHandler* SoundBufferHandler::GetInstance()
 {
-	static SoundBufferHandler* instance;
-	if( instance == nullptr )
-	{
-		instance = new SoundBufferHandler();
-	}
-	return instance;
+	static SoundBufferHandler instance;
+	return &instance;
 }

596bc30a484f1d34f591c2645d1ed7e8d99c415a
Gnidleif <gnidleif@gmail.com>
2015-02-05 00:26:53 +0100

    Main-features
    You can now choose to start a server AND client connection, or a server
    OR client connection.

--- Networks/Network3/Network3/Main.cpp
+++ Networks/Network3/Network3/Main.cpp
@@ -3,6 +3,9 @@
 #include "Server.h"
 #include "Client.h"
 
+int StartServerAndClient();
+int StartServerOrClient();
+
 int main()
 {
 #if defined(DEBUG) | defined(_DEBUG)
@@ -19,76 +22,97 @@ int main()
 	EF::REGISTER_EVENT( Event_Remote_Update );
 
 	std::string answer = "";
+	std::cout << "Would you like to" << std::endl;
+	std::cout << "(1) start a server AND client connection?" << std::endl;
+	std::cout << "(2) start a server OR client connection?" << std::endl;
+
+	std::getline( std::cin, answer );
+	if( answer == "1" )
+	{
+		return StartServerAndClient();
+	}
+	else if( answer == "2" )
+	{
+		return StartServerOrClient();
+	}
+	else
+	{
+		EventManager::GetInstance()->Release();
+		OutputDebugStringA( "Wrong input, douchebag." );
+		return 3;
+	}
+
+	return 0;
+}
+
+int StartServerAndClient()
+{
+	std::string ip	= "127.0.0.1";
+	UINT port		= 27015;
+
+	Server* server = nullptr;
+	Client* client = nullptr;
+
+	server = new Server();
+	if( !server->Initialize(port) )
+	{
+		if( server )
+			server->Release();
+		SAFE_DELETE( server );
+		EventManager::GetInstance()->Release();
+		return 1;
+	}
+
+	client = new Client();
+	if( !client->Initialize( ip, port ) )
+	{
+		if( client )
+			client->Release();
+		SAFE_DELETE( client );
+
+		if( server )
+			server->Release();
+		SAFE_DELETE( server );
+
+		EventManager::GetInstance()->Release();
+		return 2;
+	}
+
+	int frameCount = 0;
+	while( !GetAsyncKeyState( VK_ESCAPE ) )
+	{
+		frameCount++;
+		client->DoSelect( 0 );
+		server->DoSelect( 0 );
+
+		if( frameCount > 10000 )
+		{
+			client->Update( 0.0f );
+			server->Update( 0.0f );
+			frameCount = 0;
+		}
+
+		EventManager::GetInstance()->Update();
+	}
+
+	if( client )
+		client->Release();
+	SAFE_DELETE( client );
+
+	if( server )
+		server->Release();
+	SAFE_DELETE( server );
+	EventManager::GetInstance()->Release();
+	return 0;
+}
+int StartServerOrClient()
+{
+	std::string answer = "";
 	std::cout << "Would you like to start a (S)erver or a (C)lient? ";
 	std::getline( std::cin, answer );
 
 	std::string ip	= "127.0.0.1";
 	UINT port		= 27015;
-
-	//bool serverOn = false;
-
-	//Server* server = nullptr;
-	//Client* client = nullptr;
-	//if( answer == "S" || answer == "s" )
-	//{
-	//	server = new Server();
-	//	if( !server->Initialize(port) )
-	//	{
-	//		if( server )
-	//			server->Release();
-	//		SAFE_DELETE( server );
-	//		EventManager::GetInstance()->Release();
-	//		return 1;
-	//	}
-	//	serverOn = true;
-	//}
-
-	//client = new Client();
-	//if( !client->Initialize( ip, port ) )
-	//{
-	//	if( client )
-	//		client->Release();
-	//	SAFE_DELETE( client );
-
-	//	if( server )
-	//		server->Release();
-	//	SAFE_DELETE( server );
-
-	//	EventManager::GetInstance()->Release();
-	//	return 2;
-	//}
-
-	//int frameCount = 0;
-	//while( !GetAsyncKeyState( VK_ESCAPE ) )
-	//{
-	//	frameCount++;
-	//	client->DoSelect( 0 );
-	//	if( serverOn )
-	//	{
-	//		server->DoSelect( 0 );
-	//	}
-	//	if( frameCount > 10000 )
-	//	{
-	//		client->Update( 0.0f );
-	//		if( serverOn )
-	//		{
-	//			server->Update( 0.0f );
-	//		}
-	//		frameCount = 0;
-	//	}
-
-	//	EventManager::GetInstance()->Update();
-	//}
-
-	//if( client )
-	//	client->Release();
-	//SAFE_DELETE( client );
-
-	//if( server )
-	//	server->Release();
-	//SAFE_DELETE( server );
-
-	//EventManager::GetInstance()->Release();
 
 	Network* network = nullptr;
 	if( answer == "S" || answer == "s" )

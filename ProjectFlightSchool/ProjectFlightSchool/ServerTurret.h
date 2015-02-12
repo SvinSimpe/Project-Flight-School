145af94675e6cca6cd2e83b1d618f497b0f31ba3
Gnidleif <gnidleif@gmail.com>
2015-02-12 16:47:55 +0100

    Some continuation 	Pew banana brunsås

--- ProjectFlightSchool/ProjectFlightSchool/ServerTurret.h
+++ ProjectFlightSchool/ProjectFlightSchool/ServerTurret.h
@@ -4,6 +4,36 @@
 class ServerTurret
 {
 	private:
+		enum class TurretMode
+		{
+			Idle,
+			Attack,
+		};
+
+		class ITurretMode
+		{
+		public:
+			virtual void Action( ServerTurret* t ) = 0;
+					ITurretMode() {}
+			virtual ~ITurretMode() {}
+		};
+
+		class IdleTurret : public ITurretMode
+		{
+		public:
+			void Action( ServerTurret* t );
+			IdleTurret() : ITurretMode() {}
+			~IdleTurret() {}
+		};
+
+		class AttackingTurret : public ITurretMode
+		{
+		public:
+			void Action( ServerTurret* t );
+			AttackingTurret() : ITurretMode() {}
+			~AttackingTurret() {}
+		};
+
 	protected:
 	public:
 

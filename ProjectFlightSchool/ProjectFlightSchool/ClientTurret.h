82364ed743bc5ece75e3242a6ae3e827ecf0a58d
Gnidleif <gnidleif@gmail.com>
2015-02-12 16:37:05 +0100

    Server and Client turret pew pew 	Forgot to pull development -.-

---
+++ ProjectFlightSchool/ProjectFlightSchool/ClientTurret.h
@@ -0,0 +1,12 @@
+#ifndef CLIENTTURRET_H
+#define CLIENTTURRET_H
+
+#include "ServerTurret.h"
+
+class ClientTurret
+{
+public:
+	ClientTurret();
+	~ClientTurret();
+};
+#endif
---
+++ ProjectFlightSchool/ProjectFlightSchool/ServerTurret.h
@@ -0,0 +1,16 @@
+#ifndef SERVERTURRET_H
+#define SERVERTURRET_H
+
+class ServerTurret
+{
+	private:
+	protected:
+	public:
+
+	private:
+	protected:
+	public:
+		ServerTurret();
+		virtual ~ServerTurret();
+};
+#endif

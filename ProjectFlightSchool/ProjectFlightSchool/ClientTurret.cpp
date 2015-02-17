a5bd7f6389964cf9922d146f1b50d175ae2960ef
Gnidleif <gnidleif@gmail.com>
2015-02-17 17:00:15 +0100

    Idle rotation implemented 	AGAIN!

--- ProjectFlightSchool/ProjectFlightSchool/ClientTurret.cpp
+++ ProjectFlightSchool/ProjectFlightSchool/ClientTurret.cpp
@@ -29,41 +29,14 @@ void ClientTurret::FindTarget( BoundingCircle* targetCircle )
 
 void ClientTurret::Render( float deltaTime, DirectX::XMFLOAT4X4 parentWorld )
 {
-	// Calculcates draw stuff for the Base
 	float yaw = atan2f( mTurretHead->rot.z, mTurretHead->rot.x );
-	
-	DirectX::XMMATRIX parent		= DirectX::XMLoadFloat4x4( &parentWorld );
-
-	DirectX::XMMATRIX transformation = DirectX::XMMatrixAffineTransformation( 
-		DirectX::XMLoadFloat3( &mScale ), 
-		DirectX::XMVectorZero(), 
-		DirectX::XMLoadFloat4( &mRot ), 
-		DirectX::XMLoadFloat3( &mPos ) );
-
-	DirectX::XMMATRIX world = transformation * parent;
-	DirectX::XMFLOAT4X4 worldFinished;
-	DirectX::XMStoreFloat4x4( &worldFinished, world );
-
-	RenderManager::GetInstance()->AddObject3dToList( mAssetID, worldFinished );
-
-	// Calculates draw stuff for the TurretHead
-
-	XMFLOAT3 turretHeadRot3 = XMFLOAT3( mTurretHead->rot.x, mTurretHead->rot.y, mTurretHead->rot.z );
 	float roll	= -XMVectorGetX( 
 					XMVector3AngleBetweenVectors( 
 					XMLoadFloat3( &XMFLOAT3(  mTurretHead->rot.x, 0.0f, mTurretHead->rot.z ) ),
-					XMLoadFloat3( &turretHeadRot3 ) ) );
+					XMLoadFloat4( &mTurretHead->rot ) ) );
 
-	transformation = DirectX::XMMatrixAffineTransformation( 
-		DirectX::XMLoadFloat3( &mScale ), 
-		DirectX::XMVectorZero(), 
-		DirectX::XMLoadFloat4( &mTurretHead->rot ), 
-		DirectX::XMLoadFloat3( &mTurretHead->pos ) );
-
-	world = transformation * parent;
-	XMStoreFloat4x4( &worldFinished, world );
-
-	RenderManager::GetInstance()->AddObject3dToList( mTurretHead->model, worldFinished );
+	RenderManager::GetInstance()->AddObject3dToList( mAssetID, mPos, XMFLOAT3( 0.0f, -yaw, 0.0f ) );
+	RenderManager::GetInstance()->AddObject3dToList( mTurretHead->model, mTurretHead->pos, XMFLOAT3( 0.0f, -yaw, roll ) );
 }
 
 void ClientTurret::Update( float deltaTime )
@@ -74,28 +47,11 @@ void ClientTurret::Update( float deltaTime )
 
 void ClientTurret::Reset( UINT id, UINT team, XMFLOAT3 pos, XMFLOAT4 rot, XMFLOAT3 scale )
 {
-	ServerTurret::Reset( id, team, pos, rot, scale );
-
-	SwitchMode( IDLE_MODE );
-	mTargetCircle		= nullptr;
-	mScanCircle->center = mPos;
-
-	mLoadOut->rangedWeapon->Reset();
-
-	mPos.y					+= 2.0f;
-
-	mTurretHead->pos		= pos;
-	mTurretHead->pos.y		+= 2.0f;
-	mTurretHead->rot		= rot;
-	mTurretHead->scale		= scale;
+	// IMPLEMENT RESET LATER
 }
 
 void ClientTurret::Initialize( UINT id, UINT team, XMFLOAT3 pos, XMFLOAT4 rot, XMFLOAT3 scale )
 {
-	pos.x += -1.8f;
-	pos.y += 2.3f;
-	pos.z += 1.0f;
-	rot.y += 1.0f;
 	ServerTurret::Initialize( id, team, pos, rot, scale );
 
 	SwitchMode( IDLE_MODE );
@@ -106,13 +62,20 @@ void ClientTurret::Initialize( UINT id, UINT team, XMFLOAT3 pos, XMFLOAT4 rot, X
 	mLoadOut				= new LoadOut();
 	mLoadOut->rangedWeapon	= new RangedInfo( MINIGUN );
 
-	mPos.y					+= 1.0f;
+	mPos.x -= 1.798f;
+	mPos.y += 3.878f;
+	mPos.z += 1.609f;
+
+	mRot.x = 1.0f;
+	mRot.y = 0.0f;
+	mRot.z = 0.0f;
+
 	Graphics::GetInstance()->LoadStatic3dAsset( "../Content/Assets/PermanentAssets/Turret/", "turretBase2.pfs", mAssetID );
 
 	mTurretHead				= new TurretPart();
 	Graphics::GetInstance()->LoadStatic3dAsset( "../Content/Assets/PermanentAssets/Turret/", "turret.pfs", mTurretHead->model );
 	mTurretHead->pos		= mPos;
-	mTurretHead->pos.y		+= 0.6f;
+	mTurretHead->pos.y		+= 1.2f;
 	mTurretHead->rot		= mRot;
 	mTurretHead->scale		= mScale;
 }

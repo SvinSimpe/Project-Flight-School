441ff6dadc25a12df042bf024283292ff8b7f0f8
Mioa <mikael_o89@hotmail.com>
2015-02-24 16:49:01 +0100

    Update Main.cpp

--- ProjectFlightSchool/ProjectFlightSchool/Main.cpp
+++ ProjectFlightSchool/ProjectFlightSchool/Main.cpp
@@ -18,20 +18,20 @@ int WINAPI wWinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdL
 	//_CrtSetBreakAlloc(1764133); // Break at specific memory allocation point
 #endif
 	// Console window code
-	if( AllocConsole() )
-	{
-		HANDLE handle_out = GetStdHandle( STD_OUTPUT_HANDLE );
-		int hCrt = _open_osfhandle( (long)handle_out, _O_TEXT );
-		FILE* hf_out = _fdopen( hCrt, "w" );
-		setvbuf( hf_out, nullptr, _IONBF, 1 );
-		*stdout = *hf_out;
-
-		HANDLE handle_in = GetStdHandle( STD_INPUT_HANDLE );
-		hCrt = _open_osfhandle( (long)handle_in, _O_TEXT );
-		FILE* hf_int = _fdopen( hCrt, "r" );
-		setvbuf( hf_int, nullptr, _IONBF, 128 );
-		*stdin = *hf_int;
-	}
+	//if( AllocConsole() )
+	//{
+	//	HANDLE handle_out = GetStdHandle( STD_OUTPUT_HANDLE );
+	//	int hCrt = _open_osfhandle( (long)handle_out, _O_TEXT );
+	//	FILE* hf_out = _fdopen( hCrt, "w" );
+	//	setvbuf( hf_out, nullptr, _IONBF, 1 );
+	//	*stdout = *hf_out;
+	//
+	//	HANDLE handle_in = GetStdHandle( STD_INPUT_HANDLE );
+	//	hCrt = _open_osfhandle( (long)handle_in, _O_TEXT );
+	//	FILE* hf_int = _fdopen( hCrt, "r" );
+	//	setvbuf( hf_int, nullptr, _IONBF, 128 );
+	//	*stdin = *hf_int;
+	//}
 	// End of console window code
 
 	int localReturn = 0;

d084a7098bb505eb27a506845e19e93471d7375c
svekai <karinsve90@hotmail.com>
2015-02-26 15:42:37 +0100

    LobbyOwnerState

---
+++ ProjectFlightSchool/ProjectFlightSchool/LobbyOwnerState.cpp
@@ -0,0 +1,59 @@
+#include "LobbyOwnerState.h"
+
+void LobbyOwnerState::HandleInput()
+{
+	if( mStartButton.LeftMousePressed() )
+	{
+		IEventPtr E1( new Event_Client_Lobby_Finished() );
+		Client::GetInstance()->SendEvent( E1 );
+	}
+}
+
+HRESULT LobbyOwnerState::Update( float deltaTime )
+{
+	HRESULT hr = LobbyState::Update( deltaTime );
+
+	mStartButton.Update( deltaTime );
+	
+	HandleInput();
+
+	return hr;
+}
+
+HRESULT LobbyOwnerState::Render()
+{
+
+	mStartButton.Render();
+	
+	HRESULT hr = LobbyState::Render();
+	return hr;
+}
+
+void LobbyOwnerState::OnExit()
+{
+	LobbyState::OnExit();
+	mStartButton.SetExitCooldown();
+}
+
+HRESULT LobbyOwnerState::Initialize()
+{
+	mStartButton.Initialize( "../Content/Assets/Textures/Menu/Create_Menu_Text/MultiPlayer.png", 1600, 700, 200, 200 );
+
+	return LobbyState::Initialize();
+}
+
+void LobbyOwnerState::Release()
+{
+	LobbyState::Release();
+	mStartButton.Release();
+}
+
+LobbyOwnerState::LobbyOwnerState()
+{
+	LobbyState::LobbyState();
+}
+
+LobbyOwnerState::~LobbyOwnerState()
+{
+
+}

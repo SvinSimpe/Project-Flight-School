da81c81418c6dbac7ee623f4e581505a791a8b21
maxilator <maxilator@github.com>
2015-03-11 10:18:39 +0100

    changes

--- ProjectFlightSchool/ProjectFlightSchool/WeaponLoadoutInfo.h
+++ ProjectFlightSchool/ProjectFlightSchool/WeaponLoadoutInfo.h
@@ -17,7 +17,7 @@
 #define MINIGUN_RANGE		0.3f								// Range
 #define MINIGUN_SPREAD		5.0f								// Spread
 #define MINIGUN_AOE			0.0f								// Area of effect
-#define MINIGUN_SPEED		30.0f								// Projectile Speed 30
+#define MINIGUN_SPEED		38.0f								// Projectile Speed 30
 #define MINIGUN_OFFSETS		XMFLOAT3( 0.75f, 0.5f, 1.4f )		// Offsets forward, right, up
 #define MINIGUN_OVERHEAT_CD 2.0f								// Cooldown that occurs when the minigun overheats	
 #define MINIGUN_OVERHEAT	1.8f								// Amount of heat the minigun get's for firing once

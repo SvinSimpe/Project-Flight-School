955a69e98d32b5dbded5083551fe996a6819bed0
Tobias Grundel <tobias.grundel@hotmail.com>
2015-03-12 09:48:07 +0100

    Weapon animation speed increased 	Hammer and claymore changed, saw unchanged. Blowtorch not needed.

--- ProjectFlightSchool/ProjectFlightSchool/RemotePlayer.cpp
+++ ProjectFlightSchool/ProjectFlightSchool/RemotePlayer.cpp
@@ -395,7 +395,19 @@ HRESULT RemotePlayer::Update( float deltaTime )
 			if( mRightArmAnimationCompleted && mArms.rightArm.mNextAnimation != mWeaponAnimations[mLoadOut->rangedWeapon->weaponType][IDLE] )
 				RenderManager::GetInstance()->AnimationStartNew( mArms.rightArm, mWeaponAnimations[mLoadOut->rangedWeapon->weaponType][IDLE] );
 
-			RenderManager::GetInstance()->AnimationUpdate( mArms.leftArm, deltaTime );
+			if( mLoadOut->meleeWeapon->weaponType == CLAYMORE )
+			{
+				RenderManager::GetInstance()->AnimationUpdate( mArms.leftArm, deltaTime * CLAYMORE_SPEED_INCREASE );
+			}
+			else if( mLoadOut->meleeWeapon->weaponType == HAMMER )
+			{
+				RenderManager::GetInstance()->AnimationUpdate( mArms.leftArm, deltaTime * HAMMER_SPEED_INCREASE );
+			}
+			else
+			{
+				RenderManager::GetInstance()->AnimationUpdate( mArms.leftArm, deltaTime );
+			}
+
 			RenderManager::GetInstance()->AnimationUpdate( mArms.rightArm, deltaTime );
 		}
 		else

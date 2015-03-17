e3baa4ff407b8070f0773c913556b978482b932b
maxilator <maxilator@github.com>
2015-03-17 13:41:22 +0100

    fixed saw

--- ProjectFlightSchool/ProjectFlightSchool/RemotePlayer.cpp
+++ ProjectFlightSchool/ProjectFlightSchool/RemotePlayer.cpp
@@ -239,13 +239,13 @@ HRESULT RemotePlayer::InitializeGraphics()
 		OutputDebugString( L"\nERROR loading player model\n" );
 
 	//CHAINSAW
-		if( FAILED( Graphics::GetInstance()->LoadAnimationAsset( "../Content/Assets/PermanentAssets/Robot/Weapons/Saw/Animations/", "sawAnim1.PaMan",	mWeaponAnimations[SAW][ATTACK] ) ) )
+		if( FAILED( Graphics::GetInstance()->LoadAnimationAsset( "../Content/Assets/PermanentAssets/Robot/Weapons/Saw/Animations/", "SawAttack.PaMan",	mWeaponAnimations[SAW][ATTACK] ) ) )
 		OutputDebugString( L"\nERROR loading chainsaw animation\n" );
 
-		if( FAILED( Graphics::GetInstance()->LoadAnimationAsset( "../Content/Assets/PermanentAssets/Robot/Weapons/Saw/Animations/", "sawIdle.PaMan",	mWeaponAnimations[SAW][IDLE] ) ) )
+		if( FAILED( Graphics::GetInstance()->LoadAnimationAsset( "../Content/Assets/PermanentAssets/Robot/Weapons/Saw/Animations/", "SawIdle.PaMan",	mWeaponAnimations[SAW][IDLE] ) ) )
 		OutputDebugString( L"\nERROR loading chainsaw animation\n" );
 
-		if( FAILED( Graphics::GetInstance()->LoadAnimationAsset( "../Content/Assets/PermanentAssets/Robot/Weapons/Saw/Animations/", "sawWalk.PaMan",	mWeaponAnimations[SAW][WALK] ) ) )
+		if( FAILED( Graphics::GetInstance()->LoadAnimationAsset( "../Content/Assets/PermanentAssets/Robot/Weapons/Saw/Animations/", "SawIdle.PaMan",	mWeaponAnimations[SAW][WALK] ) ) )
 		OutputDebugString( L"\nERROR loading chainsaw animation\n" );
 
 	//////////////////////////////////////
